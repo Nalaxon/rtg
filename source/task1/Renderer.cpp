695ef123d108d6239e26ab6a9e388f2aae86b83f
Narcanti <bernhard.rapp@student.tugraz.at>
2014-11-01 19:50:11 +0100

    draw triangle

--- source/task1/Renderer.cpp
+++ source/task1/Renderer.cpp
@@ -2,14 +2,24 @@
 
 
 #include "Renderer.h"
-
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <GL\GL.h>
 
 Renderer::Renderer(GL::platform::Window& window)
 	: context(window)
 {
+        
+	
 	glClearColor(0.1f, 0.3f, 1.0f, 1.0f);
 
 	window.attach(this);
+
+
+
 }
 
 void Renderer::resize(int width, int height)
@@ -21,8 +31,176 @@ void Renderer::resize(int width, int height)
 void Renderer::render()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-
 	glViewport(0, 0, viewport_width, viewport_height);
 
+	
+	// Draw the triangle !
+	glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
 	context.swapBuffers();
 }
+
+
+void loadshader(const char* filename, std::string &ShaderSource)
+{
+    std::ifstream file;
+
+    
+	std::ifstream ShaderStream(filename, std::ios::in);
+	if (ShaderStream.is_open())
+	{
+		std::string Line = "";
+		while (std::getline(ShaderStream, Line))
+			ShaderSource += "\n" + Line;
+		ShaderStream.close();
+	}
+
+}
+
+int unloadshader(GLubyte** ShaderSource)
+{
+    if (*ShaderSource != 0)
+	delete[] *ShaderSource;
+    *ShaderSource = 0;
+	return 0;
+}
+
+void Renderer::createShader(const char* vertexShaderPath, const char* fragmentShaderPath)
+{
+    std::cout << "entered setUpShader()" << std::endl;
+	GLint result = GL_FALSE;
+	int infoLogLength;
+	std::string vertexShaderSource, fragmentShaderSource;
+
+	loadshader(vertexShaderPath, vertexShaderSource);
+	loadshader(fragmentShaderPath, fragmentShaderSource);
+	
+	vertexShaderObject = glCreateShader(GL_VERTEX_SHADER);
+	fragmentShaderObject = glCreateShader(GL_FRAGMENT_SHADER);
+
+	const char* const_vss = vertexShaderSource.c_str();
+	const char* const_vfs = fragmentShaderSource.c_str();
+
+	glShaderSource(vertexShaderObject, 1, &const_vss, NULL);
+	glCompileShader(vertexShaderObject);
+	
+
+	// Check Vertex Shader
+	glGetShaderiv(vertexShaderObject, GL_COMPILE_STATUS, &result);
+	glGetShaderiv(vertexShaderObject, GL_INFO_LOG_LENGTH, &infoLogLength);
+	std::vector<char> VertexShaderErrorMessage(infoLogLength);
+	glGetShaderInfoLog(vertexShaderObject, infoLogLength, NULL, &VertexShaderErrorMessage[0]);
+
+	glShaderSource(fragmentShaderObject, 1, &const_vfs, NULL);
+	glCompileShader(fragmentShaderObject);
+
+	// Check Fragment Shader
+	glGetShaderiv(fragmentShaderObject, GL_COMPILE_STATUS, &result);
+	glGetShaderiv(fragmentShaderObject, GL_INFO_LOG_LENGTH, &infoLogLength);
+	std::vector<char> FragmentShaderErrorMessage(infoLogLength);
+	glGetShaderInfoLog(fragmentShaderObject, infoLogLength, NULL, &FragmentShaderErrorMessage[0]);
+
+	programmID = glCreateProgram();
+
+	glAttachShader(programmID, vertexShaderObject);
+	glAttachShader(programmID, fragmentShaderObject);
+
+	glLinkProgram(programmID);
+
+	// Check the program
+	glGetProgramiv(programmID, GL_LINK_STATUS, &result);
+	glGetProgramiv(programmID, GL_INFO_LOG_LENGTH, &infoLogLength);
+	std::vector<char> ProgramErrorMessage(std::max(infoLogLength, int(1)));
+	glGetProgramInfoLog(programmID, infoLogLength, NULL, &ProgramErrorMessage[0]);
+
+	//glDeleteShader(vertexShaderObject);
+	//glDeleteShader(fragmentShaderObject);
+
+	glUseProgram(programmID);
+
+	GLenum ErrorCheckValue = glGetError();
+	if (ErrorCheckValue != GL_NO_ERROR)
+	{
+		fprintf(
+			stderr,
+			"ERROR: Could not create the shaders: \n"
+			//gluErrorString(ErrorCheckValue)
+			);
+		exit(-1);
+	}
+}
+
+void Renderer::destroyShader(void)
+{
+	GLenum ErrorCheckValue = glGetError();
+	glUseProgram(0);
+	glDetachShader(programmID, vertexShaderObject);
+	glDetachShader(programmID, fragmentShaderObject);
+	glDeleteShader(fragmentShaderObject);
+	glDeleteShader(vertexShaderObject);
+	glDeleteProgram(programmID);
+	ErrorCheckValue = glGetError();
+	if (ErrorCheckValue != GL_NO_ERROR)
+	{
+		fprintf(
+			stderr,
+			"ERROR: Could not destroy the shaders: \n"
+			//gluErrorString(ErrorCheckValue)
+			);
+		exit(-1);
+	}
+
+}
+
+void Renderer::createVertexBuffers(void)
+{
+	//VAO
+	glGenVertexArrays(1, &vaoId);
+	glBindVertexArray(vaoId);
+
+	//Triangle
+	GLfloat triangle[] = {
+		0.8f, -0.8f, 0.0f,
+		0.0f, 0.8f, 0.0f,
+		-0.8f, -0.8f, 0.0f
+	};
+	
+	glGenBuffers(1, &vTriangleId);
+	glBindBuffer(GL_ARRAY_BUFFER, vTriangleId);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	GLenum ErrorCheckValue = glGetError();
+	if (ErrorCheckValue != GL_NO_ERROR)
+	{
+		fprintf(
+			stderr,
+			"ERROR: Could not create a VBO: \n"
+			//glGetInfoErr(ErrorCheckValue)
+			);
+		exit(-1);
+	}
+}
+
+void Renderer::destroyVertexBuffers(void)
+{
+	GLenum ErrorCheckValue = glGetError();
+	glDisableVertexAttribArray(1);
+	glDisableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glDeleteBuffers(1, &vTriangleId);
+	glBindVertexArray(0);
+	glDeleteVertexArrays(1, &vaoId);
+	ErrorCheckValue = glGetError();
+	if (ErrorCheckValue != GL_NO_ERROR)
+	{
+		fprintf(
+			stderr,
+			"ERROR: Could not destroy the VBO:  \n"
+			//gluErrorString(ErrorCheckValue)
+			);
+		exit(-1);
+	}
+}
+
+
--- source/task1/main.cpp
+++ source/task1/main.cpp
@@ -1,7 +1,4 @@
-
-
-
-#include <iostream>
+﻿#include <iostream>
 #include <stdexcept>
 
 #include <GL/platform/Window.h>
@@ -12,25 +9,42 @@
 
 int main(int argc, char* argv[])
 {
+    //char* VERTEX_SHADER_SOURCE = ".\\..\\..\\..\\..\\source\\task1\\vertexshader.glsl";
+	//char* FRAGMENT_SHADER_SOURCE = ".\\..\\..\\..\\..\\source\\task1\\fragmentshader.glsl";
+	const char* VERTEX_SHADER_SOURCE = "./source/task1/vertexshader.glsl";
+	const char* FRAGMENT_SHADER_SOURCE = "./source/task1/fragmentshader.glsl";
 	try
 	{
-		GL::platform::Window window("Assignment 1 — The first triangle", 800, 600, 0, 0, false, 3, 3);
+		GL::platform::Window window("Assignment 1 — The first triangle", 800, 600, 0, 0, false, 1, 3);
+
 		Renderer renderer(window);
+		renderer.resize(800, 600);
+
+
+		renderer.createShader(VERTEX_SHADER_SOURCE, FRAGMENT_SHADER_SOURCE);
+		renderer.createVertexBuffers();
+
 		InputHandler input_handler;
 
 		window.attach(static_cast<GL::platform::KeyboardInputHandler*>(&input_handler));
 		window.attach(static_cast<GL::platform::MouseInputHandler*>(&input_handler));
-
+		
 		GL::platform::run(renderer);
+
+		//renderer.destroyVertexBuffers();
+		renderer.destroyShader();
+
 	}
 	catch (std::exception& e)
 	{
 		std::cout << "error: " << e.what() << std::endl;
+		getchar();
 		return -1;
 	}
 	catch (...)
 	{
 		std::cout << "unknown exception" << std::endl;
+		getchar();
 		return -128;
 	}
 
