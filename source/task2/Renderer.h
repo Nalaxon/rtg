4b7f475615f5627035518ab9422c7eb9de547278
Narcanti <bernhard.rapp@student.tugraz.at>
2014-11-01 20:35:09 +0100

    draw triangle, forgot to add

--- source/task2/Renderer.h
+++ source/task2/Renderer.h
@@ -18,6 +18,12 @@ private:
 	int viewport_width;
 	int viewport_height;
 
+	GLuint programmID;
+	GLuint vertexShaderObject;
+	GLuint fragmentShaderObject;
+	GLuint vaoId;
+	GLuint vTriangleId;
+
 public:
 	Renderer(const Renderer&) = delete;
 	Renderer& operator =(const Renderer&) = delete;
@@ -26,6 +32,10 @@ public:
 
 	void resize(int width, int height);
 	void render();
+	void createShader(const char* vertexShaderPath, const char* fragmentShaderPath);
+	void destroyShader(void);
+	void createVertexBuffers(void);
+	void destroyVertexBuffers(void);
 };
 
 #endif  // INCLUDED_RENDERER
