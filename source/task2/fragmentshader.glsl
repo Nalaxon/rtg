2e7f87d892836eab97fe71619bd9253fc90f11ae
Narcanti <bernhard.rapp@student.tugraz.at>
2014-11-02 19:41:07 +0100

    draw solid color using gl_color

--- source/task2/fragmentshader.glsl
+++ source/task2/fragmentshader.glsl
@@ -1,4 +1,7 @@
 void main()
+
 {
-  gl_FragColor = vec4(1.0, 0.4, 0.0, 1.0);
+
+  gl_FragColor = gl_Color;
+
 }
--- source/task2/vertexshader.glsl
+++ source/task2/vertexshader.glsl
@@ -1,4 +1,6 @@
 void main()
 {
 	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+        gl_FrontColor = gl_Color;
+        gl_BackColor  = gl_Color;
 }
