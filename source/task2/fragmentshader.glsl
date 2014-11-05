b4be08d94a7055887fa0187f81d9f64b3cb79e2c
Narcanti <bernhard.rapp@student.tugraz.at>
2014-11-05 20:38:16 +0100

    Get rid of obsolete stuff

--- source/task2/fragmentshader.glsl
+++ source/task2/fragmentshader.glsl
@@ -1,7 +1,9 @@
+#version 330
+in vec4 FrontColor;
+out vec4 out_FragColor;
 void main()
 
 {
-
-  gl_FragColor = gl_Color;
-
+  
+   out_FragColor = FrontColor;
 }
--- source/task2/vertexshader.glsl
+++ source/task2/vertexshader.glsl
@@ -1,7 +1,10 @@
+#version 330
+layout(location=0) in vec3 in_Vertex;
 layout(location=1) in vec4 in_Color;
+
+out vec4 FrontColor;
 void main()
 {
-	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
-        gl_FrontColor = in_Color;
-        gl_BackColor  = in_Color;
+	gl_Position = vec4(in_Vertex, 1.0f);
+        FrontColor = in_Color;
 }
